15d0abe99df396c67c1e2294d0033f38ebfeee01
Le Viet Hoang <levieth710@gmail.com>
2023-06-08 21:32:11 +0700

    update

---
+++ src/main/java/org/example/leetcode/ValidParentheses.java
@@ -0,0 +1,27 @@
+package org.example.leetcode;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == '('){
+                stack.push(')');
+            }
+
+            else if(s.charAt(i) == '['){
+                stack.push(']');
+            }
+
+            else if(s.charAt(i) == '{'){
+                stack.push('}');
+            }
+
+            else if(stack.isEmpty() || stack.pop() != s.charAt(i)){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
