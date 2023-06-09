852801476c3a7a54c9957861e0acb7266ee27e30
Le Viet Hoang <levieth710@gmail.com>
2023-06-09 16:23:27 +0700

    update

---
+++ src/main/java/org/example/leetcode/EvaluateReversePolishNotation.java
@@ -0,0 +1,31 @@
+package org.example.leetcode;
+
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0 ; i  < tokens.length ; i++){
+            if(tokens[i].equals("+")){
+                stack.add(stack.pop() + stack.pop());
+            }
+            else if(tokens[i].equals("-")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.add(b - a);
+            }
+            else if(tokens[i].equals("*")){
+                stack.add(stack.pop() * stack.pop());
+            }
+            else if(tokens[i].equals("/")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.add(b / a);
+            }
+            else{
+                stack.add(Integer.parseInt(tokens[i]));
+            }
+        }
+        return stack.peek();
+    }
+}
