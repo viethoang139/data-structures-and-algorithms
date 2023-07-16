7d2039c20606e5ef133bfbf8e500f8f1b94cf81e
Le Viet Hoang <levieth710@gmail.com>
2023-07-16 23:50:03 +0700

    update

--- src/main/java/org/example/leetcode/BestTimetoBuyandSellStock.java
+++ src/main/java/org/example/leetcode/BestTimetoBuyandSellStock.java
@@ -1,22 +1,40 @@
 package org.example.leetcode;
 
 public class BestTimetoBuyandSellStock {
-    public int maxProfit(int[] arr) {
-        int min = Integer.MAX_VALUE;
-        int ans = 0;
-        int current = 0;
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] < min){
-                min = arr[i];
+    // solution 1
+//    public int maxProfit(int[] arr) {
+//        int min = Integer.MAX_VALUE;
+//        int ans = 0;
+//        int current = 0;
+//        for(int i = 0 ; i < arr.length ; i++){
+//            if(arr[i] < min){
+//                min = arr[i];
+//            }
+//
+//            current = arr[i] - min;
+//            if(ans < current){
+//                ans = current;
+//            }
+//
+//        }
+//        return ans;
+//    }
+    // solution 2
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int i = 0 , j = 1;
+        while(j < prices.length){
+            if(prices[i] > prices[j]){
+                i = j;
+                j++;
             }
-
-            current = arr[i] - min;
-            if(ans < current){
-                ans = current;
+            else{
+                int diff = prices[j] - prices[i];
+                max = Math.max(diff , max);
+                j++;
             }
-
         }
-        return ans;
-
+        return max;
     }
+
 }
--- src/main/java/org/example/leetcode/ValidParentheses.java
+++ src/main/java/org/example/leetcode/ValidParentheses.java
@@ -1,27 +1,54 @@
 package org.example.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class ValidParentheses {
+    // solution 1
+//    public boolean isValid(String s) {
+//        Stack<Character> stack = new Stack<>();
+//        for(int i = 0 ; i < s.length() ; i++){
+//            if(s.charAt(i) == '('){
+//                stack.push(')');
+//            }
+//
+//            else if(s.charAt(i) == '['){
+//                stack.push(']');
+//            }
+//
+//            else if(s.charAt(i) == '{'){
+//                stack.push('}');
+//            }
+//
+//            else if(stack.isEmpty() || stack.pop() != s.charAt(i)){
+//                return false;
+//            }
+//        }
+//        return stack.isEmpty();
+//    }
+
+    // solution 2
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(int i = 0 ; i < s.length() ; i++){
-            if(s.charAt(i) == '('){
-                stack.push(')');
+        Map<Character , Character> map = new HashMap<>(3);
+        map.put(')' , '(');
+        map.put(']' , '[');
+        map.put('}' , '{');
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c)){
+                if(!stack.isEmpty() && map.get(c) == stack.peek()){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
             }
-
-            else if(s.charAt(i) == '['){
-                stack.push(']');
-            }
-
-            else if(s.charAt(i) == '{'){
-                stack.push('}');
-            }
-
-            else if(stack.isEmpty() || stack.pop() != s.charAt(i)){
-                return false;
+            else{
+                stack.push(c);
             }
         }
         return stack.isEmpty();
     }
+
 }
