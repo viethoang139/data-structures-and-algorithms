0822a35b9406ac1bf922c880f76e75c6b3fd0971
Le Viet Hoang <levieth710@gmail.com>
2023-08-10 13:17:49 +0700

    update

--- src/main/java/org/example/leetcode/MergeSortedArray.java
+++ src/main/java/org/example/leetcode/MergeSortedArray.java
@@ -3,18 +3,37 @@ package org.example.leetcode;
 import java.util.Arrays;
 
 public class MergeSortedArray {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+//    public void merge(int[] nums1, int m, int[] nums2, int n) {
+//
+//        int i = nums1.length-1;
+//        int j = 0;
+//        while(j < nums2.length){
+//            if(nums1[i] == 0){
+//                nums1[i] = nums2[j];
+//                i--;
+//                j++;
+//            }
+//        }
+//        Arrays.sort(nums1);
+//
+//    }
 
-        int i = nums1.length-1;
-        int j = 0;
-        while(j < nums2.length){
-            if(nums1[i] == 0){
-                nums1[i] = nums2[j];
-                i--;
-                j++;
+    // Time: O(m+n)
+    // Space: O(1)
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m-1;
+        int p2 = n-1;
+        for(int i = nums1.length -1 ; i >= 0 ; i--){
+            int value1 = p1 >= 0 ? nums1[p1] : Integer.MIN_VALUE;
+            int value2 = p2 >= 0 ? nums2[p2] : Integer.MIN_VALUE;
+            if(value1 < value2){
+                nums1[i] = value2;
+                p2--;
+            }
+            else{
+                nums1[i] = value1;
+                p1--;
             }
         }
-        Arrays.sort(nums1);
-
     }
 }
