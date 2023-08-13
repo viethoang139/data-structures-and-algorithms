5d39b2e1e673eb4267409fc2e29216cd4b803add
Le Viet Hoang <levieth710@gmail.com>
2023-08-13 11:08:38 +0700

    update

--- src/main/java/org/example/leetcode/PlusOne.java
+++ src/main/java/org/example/leetcode/PlusOne.java
@@ -1,16 +1,41 @@
 package org.example.leetcode;
 
 public class PlusOne {
+//    public int[] plusOne(int[] digits) {
+//        int[]ans = new int[digits.length + 1];
+//        for(int i = digits.length - 1 ; i >= 0 ; i--){
+//            if(digits[i] < 9){
+//                digits[i]++;
+//                return digits;
+//            }
+//            digits[i] = 0;
+//        }
+//        ans[0] = 1;
+//        return ans;
+//    }
+
     public int[] plusOne(int[] digits) {
-        int[]ans = new int[digits.length + 1];
+        int carry = 1;
         for(int i = digits.length - 1 ; i >= 0 ; i--){
-            if(digits[i] < 9){
-                digits[i]++;
-                return digits;
+            int curr = digits[i] + carry;
+            if(curr >= 10){
+                carry = 1;
+                digits[i] = curr % 10;
             }
-            digits[i] = 0;
+            else{
+                carry = 0;
+                digits[i] = curr;
+            }
         }
-        ans[0] = 1;
-        return ans;
+
+        int result[] = new int[digits.length + 1];
+        if(carry == 0){
+            return digits;
+        }
+        else{
+            result[0] = 1;
+            return result;
+        }
+
     }
 }
