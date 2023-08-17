5c1d89280c1aa3601d8735c7c9ecff29a8dfe078
Le Viet Hoang <levieth710@gmail.com>
2023-08-17 13:13:47 +0700

    update

--- src/main/java/org/example/leetcode/ProductOfArrayExceptSelf.java
+++ src/main/java/org/example/leetcode/ProductOfArrayExceptSelf.java
@@ -1,19 +1,21 @@
 package org.example.leetcode;
 
 public class ProductOfArrayExceptSelf {
+
+    // Time: O(n)
+    // Space: O(1)
     public int[] productExceptSelf(int[] nums) {
-        int[]arr = new int[nums.length];
-        int left = 1;
-        int right = 1;
-        for(int i = 0 ; i < nums.length ; i++){
-            arr[i] = left;
-            left *= nums[i];
+        int[]prefix = new int[nums.length];
+        prefix[0] = 1;
+        for(int i = 1 ; i < nums.length ; i++){
+            prefix[i] = prefix[i-1]*nums[i-1];
         }
+        int suffix = 1;
         for(int i = nums.length - 1 ; i >= 0 ; i--){
-            arr[i] *= right;
-            right *= nums[i];
+            prefix[i] = suffix * prefix[i];
+            suffix = suffix * nums[i];
         }
-        return arr;
+        return prefix;
     }
 }
 
