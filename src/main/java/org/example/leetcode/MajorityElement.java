2611f2f1413ac5185e82719572da1a43f445c2a8
Le Viet Hoang <levieth710@gmail.com>
2023-10-04 08:02:11 +0700

    update

---
+++ src/main/java/org/example/leetcode/MajorityElement.java
@@ -0,0 +1,24 @@
+package org.example.leetcode;
+
+
+public class MajorityElement {
+    // Time: O(n)
+    // Space: O(1)
+    public int majorityElement(int[] nums) {
+        int majority = nums[0];
+        int count = 1;
+        for(int i = 1 ; i <nums.length; i++){
+            if(nums[i] == majority){
+                count++;
+            }
+            else{
+                count--;
+                if(count == 0){
+                    majority = nums[i];
+                    count++;
+                }
+            }
+        }
+        return majority;
+    }
+}
