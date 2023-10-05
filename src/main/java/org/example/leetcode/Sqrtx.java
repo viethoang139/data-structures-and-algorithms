6c0116b165f94518982634c116425540df54b948
Le Viet Hoang <levieth710@gmail.com>
2023-10-05 19:32:54 +0700

    update

--- src/main/java/org/example/leetcode/Sqrtx.java
+++ src/main/java/org/example/leetcode/Sqrtx.java
@@ -1,21 +1,28 @@
 package org.example.leetcode;
 
 public class Sqrtx {
+
+    // Time: O(logn)
+    // Space: O(1)
     public int mySqrt(int x) {
-        long low = 0;
-        long high = (x/2) +1;
-        while(low < high){
-            long mid = low + (high-low)/2 + 1;
-            if(mid * mid == x){
-                return (int)mid;
+        if(x == 0){
+            return 0;
+        }
+        int ans = -1;
+        int s =1, e = x;
+        while(s <= e){
+            int mid = s + (e-s)/2;
+            if(mid == x/mid){
+                return mid;
             }
-            else if(mid *mid > x){
-                high = mid - 1;
+            else if(mid < x/mid){
+                ans = mid;
+                s = mid + 1;
             }
             else{
-                low = mid;
+                e = mid - 1;
             }
         }
-        return (int)low;
+        return ans;
     }
 }
