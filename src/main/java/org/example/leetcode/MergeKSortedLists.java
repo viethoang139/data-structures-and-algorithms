8bcd82abb7b3421fc11f26bfe7f5b4277630d20c
Le Viet Hoang <levieth710@gmail.com>
2023-10-26 22:46:34 +0700

    update

---
+++ src/main/java/org/example/leetcode/MergeKSortedLists.java
@@ -0,0 +1,40 @@
+package org.example.leetcode;
+
+public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int size = lists.length;
+        int interval = 1;
+        while(interval < size){
+            for(int i =0 ; i < size - interval; i+=2 *interval){
+                lists[i] = merge(lists[i],lists[i+interval]);
+            }
+            interval *= 2;
+        }
+        return size >0 ? lists[0]: null;
+
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                curr.next = l1;
+                curr = l1;
+                l1 = l1.next;
+            }
+            else{
+                curr.next = l2;
+                curr = l2;
+                l2 = l2.next;
+            }
+        }
+        if(l1 != null){
+            curr.next = l1;
+        }
+        else{
+            curr.next = l2;
+        }
+        return dummy.next;
+    }
+}
