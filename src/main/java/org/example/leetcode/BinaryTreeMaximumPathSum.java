e8a44e55b8f4a2f228609258755c56094e7f2863
Le Viet Hoang <levieth710@gmail.com>
2023-10-29 09:28:32 +0700

    update

---
+++ src/main/java/org/example/leetcode/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,18 @@
+package org.example.leetcode;
+
+public class BinaryTreeMaximumPathSum {
+    public int maxPathSum(TreeNode root) {
+        int[]res = {Integer.MIN_VALUE};
+        maxPathSum(root,res);
+        return res[0];
+    }
+    private int maxPathSum(TreeNode root, int[]res){
+        if(root == null){
+            return 0;
+        }
+        int left = Math.max(0,maxPathSum(root.left,res));
+        int right = Math.max(0,maxPathSum(root.right,res));
+        res[0] = Math.max(res[0], root.val + left + right);
+        return root.val + Math.max(left,right);
+    }
+}
