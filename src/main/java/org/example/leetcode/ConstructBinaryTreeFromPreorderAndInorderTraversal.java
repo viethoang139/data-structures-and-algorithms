b2ba05cfe23c7b80fdd30b8bf2ac50aa34c23c56
Le Viet Hoang <levieth710@gmail.com>
2023-10-29 08:34:20 +0700

    update

---
+++ src/main/java/org/example/leetcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,23 @@
+package org.example.leetcode;
+
+import java.util.Arrays;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length == 0 || inorder.length == 0){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        int mid = 0;
+        for(int i =0 ; i < inorder.length; i++){
+            if(inorder[i] == preorder[0]){
+                mid = i;
+            }
+        }
+        root.left = buildTree(Arrays.copyOfRange(preorder,1,mid + 1),
+                Arrays.copyOfRange(inorder,0,mid));
+        root.right = buildTree(Arrays.copyOfRange(preorder,mid + 1,preorder.length),
+                Arrays.copyOfRange(inorder,mid + 1,inorder.length));
+        return root;
+    }
+}
