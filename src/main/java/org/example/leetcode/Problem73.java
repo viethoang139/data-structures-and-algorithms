e0fc1069d63a3b7c34ab1c187260019a3a939b25
Le Viet Hoang <levieth710@gmail.com>
2024-01-06 21:23:34 +0700

    update

---
+++ src/main/java/org/example/leetcode/Problem73.java
@@ -0,0 +1,26 @@
+package org.example.leetcode;
+
+public class Problem73 {
+    public void setZeroes(int[][] matrix) {
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(row[i] != 0 || col[j] != 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+}
